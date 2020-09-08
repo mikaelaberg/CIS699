f562b46318c5e370fb569aa962a3a77d580e292f
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2020-09-08 14:56:24 -0400

    R1 edits

--- Ch1.py
+++ Ch1.py
@@ -1,2 +1,31 @@
-print('Hello, World!')
-print('Test part two, electric bugaloo!')
+#Mikaela Berg 
+#CIS699 Advanced Python 
+#Fall20
+
+#import statments:
+import random
+
+
+#R1.1
+# def is_multiple(n,m):
+#     return (int((m/n)))
+
+# #for testing: 
+# m = 16
+# n = 4
+
+# print(n, 'Goes into', m, is_multiple(n,m), 'times.')
+
+def is_multiple(n,m):
+    if (n % m):
+        print('n is a multiple of m')
+    else: 
+        print('n is not a multiple of m')
+
+#for testing: 
+m = int(input('Pick a number(m):'))
+n = int(input('Pick another'))
+
+print(is_multiple(n,m))
+
+
