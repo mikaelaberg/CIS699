1093fc9f41305eba30a15bfba7e6aebfd1d4b465
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2020-09-14 20:22:50 -0400

    complete2_4

--- Ch2_Work/Ch2_4.py
+++ Ch2_Work/Ch2_4.py
@@ -7,18 +7,31 @@ of each type.'''
 
 class Flower:
     
-    def __init__(self, name, petal, priceper, numof):
+    def __init__(self, name, petal, pricePer):
         self.name = name
         self.petal = petal
-        self.priceper = priceper
+        self.pricePer = pricePer
 
-    # def sellingCost(self):
-    #     return ('{}' '{}'.format((self.priceper) * (self.numof)))
+    def __getName__(self):
+        return self.name
+
+    def __setName__(self, name):
+        self.name = name
+
+    def __getPetal__(self):
+        return self.petal
+
+    def __setPetal__(self, petal):
+        self.petal = petal
+    
+    def __getPricePer__(self):
+        return self.pricePer
+
+    def __setPricePer__(self, name):
+        self.pricePer = pricePer
 
 flw_1 = Flower('Rose', 5, 5.50)
 flw_2 = Flower('Tulip', 3, 2.50)
 
-# print(flw_1)
-# print(flw_2)
-
-# print(flw_1.sellingCost())
+print(flw_1.__getPetal__())
+print(flw_2.__getPetal__())
