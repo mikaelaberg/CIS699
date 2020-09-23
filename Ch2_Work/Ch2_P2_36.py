49a9507fca08a73159f66d2c73e8bd811608bb8f
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2020-09-23 15:41:09 -0400

    project update

--- Ch2_Work/Ch2_P2_36.py
+++ Ch2_Work/Ch2_P2_36.py
@@ -9,24 +9,22 @@ of that type of animal, which is placed in a random empty (i.e., previously
 None) location in the list. If a bear and a fish collide, however, then the
 fish dies (i.e., it disappears)."""
 
-
-#Possible modifications I want to make: 
-#1. Put into a text file for easier visualiztion?? 
-#2. And/Or maybe make it a button of some sorts??
-
-
 #Current Issues:
 #the variables are there but what is showing up 
 #is their location not the actualy contents 
 #maybe missing () somewhere in the long list of else statments??
 
 
-#in the time step every object should move to an adjacent place in 
+# My Notes Fpr the Program:
+#
+# in the time step every object should move to an adjacent place in 
 # in the list, longer list 
-
+# 
+# 
 # crude animation; print one loop thorugh the list ; could also be a debugger
 # make this 2d could make this like a game of life, where I walk though every 
 # cell, or follow the animals
+# 
 # 
 # can the animal move mulitple times or just once, its unrealistic that they can only move once
 # every andimal gets a chance to move on the list. 
@@ -36,16 +34,15 @@ fish dies (i.e., it disappears)."""
 # I can make the rules for the end of the box/beginging 
 # of the box 
 
-
-
-
 import random
 import numpy as np
+import matplotlib as plt
+
 
 class animal():
 
     def __init__(self, place):
-        self.place = place      #an animals place in the food chain
+        self.place = place      #place animal in the food chain
 
     def moveAnimal(self):
         return self.place
@@ -53,7 +50,7 @@ class animal():
 class bear(animal):
 
     def __placeBear__(self):
-        return "Bear(self.place)"
+        return "Bear(%s)" % self.place
 
     def movement(self):
         step = np.random.choice([-1,1])
@@ -64,7 +61,7 @@ class bear(animal):
 class fish(animal):
 
     def __placeFish__(self):
-        return "Fish(self.place)"
+        return "Fish(%s)" % self.place
 
     def movement(self):
         step = np.random.choice([-1,1])
@@ -72,43 +69,44 @@ class fish(animal):
         print(self, 'moves', 'left' if step == -1 else 'right')
         return new_place
 
+
 class river:
 
-    def __init__(self, space):
-        self.space = space
-        self.ecosys = None
+    def __init__(self, spot):
+        self.spot = spot
+        ecosys = None
 
     def initalize(self):
         self.ecosys = []
-        animal = np.random.choice ([bear, fish, None], size=self.space)
+        animal = np.random.choice ([bear, fish, None], size=self.spot)
         for place, animal in enumerate(animal):
-            self.ecosys.append(animal(((place))) if animal is not None else None)
+            self.ecosys.append(animal(place) if animal is not None else None)
 
     def timeStep(self, t=1, text=True):
         for i in range(t):
-            move_place = np.random.choice(list(range(self.space)))
+            move_place = np.random.choice(list(range(self.spot)))
             if self.ecosys[move_place] is None:
                 print ('No movement')
                 pass
             else:
-                new_space = self.ecosys[move_place].moveAnimal()
-                if new_space < 0 or new_space > len(self.ecosys) -1:
+                new_spot = self.ecosys[move_place].moveAnimal()
+                if new_spot < 0 or new_spot > len(self.ecosys) -1:
                     pass
                 elif isinstance(self.ecosys[move_place], bear):
-                    if isinstance(self.ecosys[new_space], bear):
+                    if isinstance(self.ecosys[new_spot], bear):
                         pass
-                    elif isinstance(self.ecosys[new_space], fish):
-                        self.ecosys[new_space] = bear(new_space)
+                    elif isinstance(self.ecosys[new_spot], fish):
+                        self.ecosys[new_space] = bear(new_spot)
                         self.ecosys[move_place] = None
                     else:
-                        self.ecosys[new_space] = bear(new_space)
+                        self.ecosys[new_space] = bear(new_spot)
                 elif isinstance(self.ecosys[move_place], fish):
-                    if isinstance(self.ecosys[new_space], fish):
+                    if isinstance(self.ecosys[new_spot], fish):
                         pass
-                    elif isinstance(self.ecosys[new_space], bear):
+                    elif isinstance(self.ecosys[new_spot], bear):
                         self.ecosys[move_place] = None
                     else:
-                        self.ecosys[new_space] = fish(new_space)
+                        self.ecosys[new_space] = fish(new_spot)
                 else:
                     raise ValueError("Undefined Creature")
             if text:
@@ -121,9 +119,9 @@ class river:
         print('===================')
 
 
-print(bear)
-print(fish)
-print(river)
+print(bear(3))
+print(fish(5))
+print(river(5))
 
 
 
