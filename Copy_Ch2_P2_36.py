f50785ca9de4ed3c41ca1c5909f62d4016731265
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2020-10-04 20:22:37 -0400

    Project update

--- Copy_Ch2_P2_36.py
+++ Copy_Ch2_P2_36.py
@@ -10,25 +10,15 @@ None) location in the list. If a bear and a fish collide, however, then the
 fish dies (i.e., it disappears)."""
 
 #Current Improvemnts:
-
 # GW list:
-# TODO Fish and Bears coming back into the list/view point 
-        # Torrus --> have them reenter/re appear 
-        # (get.num function?) print at each timsetp each time the calss is called
-# TODO Track the number of bears/Fish in each timesetp and graph it 
-# TODO Cacualte/Predict/Print how long it would take till the populaiton 
-        # of fish (or bears?) reacches zero (connects to gonna need a bigger ecosystem)
-
+# TODO Average of the full run times:
+    # how long on average would it take to mkae the list full of an animal type: 
+    # 1.fullList will have to be converted to an int from a str
+    # 2.store fullList by loop over river class over and over OR store in data in a file 
+    # 2.1 in loop you need control manager function to control how its looping 
+    # 2.2 in the file - read in from file - run applicaiton a good amount of times
 # MB List:
-# TODO graphing; better labels/ key. OR better graph 
-# TODO code: "we need a bigger ecosystem", what 
-        # would happen when/if the ecosystem 
-        # is full of bears/Fish/None
-        # The above doesnt happen as long as 
-        # the list/ecosystem is large 
 # TODO Reflection of this project at the end 
-# TODO Clean up/ Better documentaion 
-
 
 from pylab import *
 import numpy.random as random
@@ -85,8 +75,7 @@ class river:
             self.ecosys.append(animal(place) if animal is not None else None)
 
         self.gdata = []
-        self.lftSideStore = []
-        self.rtSideStore = []
+        self.fullList = ''
 
         for x in range((timestep)):
             self.gdata.append([[]])
@@ -105,41 +94,40 @@ class river:
             animal.place = index
             self.ecosys[index] = animal
 
-    def append_left(self):
-        """If the animal moves left out of the viewpoint of the list"""
-        if isinstance(self.ecosys[self.timeStep.move_place], bear):
-            if isinstance(self.ecosys[self.timeStep.new_spot], bear):
-                self.add_random(bear(0))
-            elif isinstance(self.ecosys[self.timeStep.new_spot], fish):
-                self.ecosys[new_spot] = bear(self.timeStep.new_spot)
-                self.ecosys[move_place] = None
-            else:
-                self.ecosys[new_spot] = bear(self.timeStep.new_spot)
-                self.ecosys[move_place] = None
 
-        elif isinstance(self.ecosys[self.timeStep.move_place], fish):
-            if isinstance(self.ecosys[self.timeStep.new_spot], fish):
-                self.add_random(fish(0))
-            elif isinstance(self.ecosys[self.timeStep.new_spot], bear):
-                self.ecosys[move_place] = None
+    def manageAnimalInteraction(self, orgin, destinationIndex, finalDestination, animalCreationSpot, animalDeletionSpot):
+        """
+        Manages animal encounters in the ecosystem and storage lists. 
+
+        orgin:                  The animals place in the list at the timestep. 
+        destinationtIndex:      The index that the animal is goig to attempt to move to.
+        finalDestination:       The index that the animal has moved to after testing for other animal types.
+        animalCreationSpot:     An empty spot in the list where a new animal will be created.
+        animalDeletionSpot:     After the animal moves its spot in the index is replace with a None
+
+        """
+
+        if isinstance(orgin, bear):
+            if isinstance(finalDestination, bear):
+                self.add_random(bear(0))
+            elif isinstance(finalDestination, fish):
+                animalCreationSpot = bear(destinationIndex)
+                animalDeletionSpot = None
             else:
-                self.ecosys[new_spot] = fish(self.timeStep.new_spot)
-                self.ecosys[move_place] = None
+                animalCreationSpot = bear(destinationIndex)
+                animalDeletionSpot = None
+
+        elif isinstance(orgin, fish):
+            if isinstance(finalDestination, fish):
+                self.add_random(fish(0))
+            elif isinstance(finalDestination, bear):
+                animalDeletionSpot = None
+            else:
+                animalCreationSpot = fish(destinationIndex)
+                animalDeletionSpot = None
 
         else:
             raise ValueError("Undefined Creature")
-
-        
-        
-        self.lftSideStore.append()
-        pass
-
-    def append_right(self):
-        """If the animal moves right out of the viewpoint of the list"""
-                         
-        self.rtSideStore.append()
-        pass
-
 
     def timeStep(self, t=1, text=True):
         """A point in time what the ecosystem looks like"""
@@ -153,12 +141,8 @@ class river:
 
             else:
                 new_spot = self.ecosys[move_place].movement()
-                if new_spot < 0:
-                    self.append_left
-
-                elif new_spot > len(self.ecosys) -1:
-                    self.append_right
-                   
+                if new_spot < 0 or new_spot > len(self.ecosys) -1:
+                    self.ecosys[move_place] = None
                 elif isinstance(self.ecosys[move_place], bear):
                     if isinstance(self.ecosys[new_spot], bear):
                         self.add_random(bear(0))
@@ -177,16 +161,44 @@ class river:
                     else:
                         self.ecosys[new_spot] = fish(new_spot)
                         self.ecosys[move_place] = None
-
                 else:
                     raise ValueError("Undefined Creature")
+                # else:
+                    # orgin = self.ecosys[move_place]
+                    # destinationIndex = new_spot
+                    # finalDestination = self.ecosys[destinationIndex]
+                    # animalCreationSpot = self.ecosys[new_spot]
+                    # animalDeletionSpot = self.ecosys[move_place]
+
+                    # self.manageAnimalInteraction(orgin, destinationIndex, finalDestination, animalCreationSpot, animalDeletionSpot)
 
             if text:
                 self.display()
-         
+
+
+            bearCount = len([i for i, x in enumerate(self.ecosys) if isinstance(x,bear)])
+
+            fishCount = len([i for i, x in enumerate(self.ecosys) if isinstance(x,fish)])
+
+            print('Total number of bears in the ecosystem: ' , bearCount)
+            print('Total number of fish in the ecosystem: ', fishCount)
+                        
             self.gdata[self.itterationCount] = self.parsecosys()
 
+            if self.fullList != '':
+                pass
+            elif bearCount == len(self.ecosys):
+                self.fullList =  'The list became full of bears at ' + str(self.itterationCount) + ' timestep. '
+
+            elif fishCount == len(self.ecosys):
+                self.fullList =  'The list became full of fish at ' + str(self.itterationCount) + ' timestep. '
+
+
             self.itterationCount += 1
+
+        print (self.fullList)
+
+
          
     def parsecosys(self):
         """Converting the animals to their associated colors for graphing"""
@@ -197,7 +209,7 @@ class river:
                 result.append(0.0)
             else:
                 result.append(self.ecosys[x].color)
-        return [result]
+        return [result[1:len(self.ecosys)-1]]
 
     def graphEcosy(self):
         """Graphing the ecosystem"""
@@ -223,10 +235,11 @@ class river:
         print('===================')
         print('Ecosystem status: \n')
         print(self.ecosys, '\n')
-        print('===================')
+        print('===================') 
 
+ 
 itterations = 50
-River = river(100)
+River = river(50)
 River.initalize(itterations)
 River.display()
 River.timeStep(itterations)
