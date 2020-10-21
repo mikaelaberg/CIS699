08ee8b445b6a0ae86a481d114a452377522f532e
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2020-10-21 19:28:37 -0400

    reading the phychl

--- GA/Heatmap Data Vis/Inital_Test.py
+++ GA/Heatmap Data Vis/Inital_Test.py
@@ -3,4 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+phycChlSigEvents = pd.read_excel(r"C:\Users\Mikaela\Desktop\F20\CIS699\CIS699\CIS699-1\GA\Heatmap Data Vis\Phyc Chl Significant Events.xlsx", sheet_name = None)
+
 
