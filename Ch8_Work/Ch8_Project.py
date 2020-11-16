d020eea5d76ae9ca5e0da456be570d777308b8de
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2020-11-15 21:29:29 -0500

    project and perosnal

--- Ch8_Work/Ch8_Project.py
+++ Ch8_Work/Ch8_Project.py
@@ -16,6 +16,19 @@
 # Use the dendrogram to try and find values of clusters of species
 # then once that is obtained use that data for a type of decision tree??
 
+
+#can I use this type of clustering to do a type of idenficiation for paralogs vs orhtologs???
+
+# Links:
+#     https://biopython.org/docs/1.75/api/Bio.Cluster.html
+#     https://biopython.org/wiki/Phylo
+#     https://github.com/shubhamjha97/hierarchical-clustering
+#     https://www.tutorialspoint.com/biopython/biopython_cluster_analysis.htm
+#     https://www-ncbi-nlm-nih-gov.ezproxy.gvsu.edu/proteinclusters
+#     https://biopython-tutorial.readthedocs.io/en/latest/notebooks/13%20-%20Phylogenetics%20with%20Bio.Phylo.html
+#     https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+
+
 #-----------------PART A----------------------------------------------------
 
 import copy
