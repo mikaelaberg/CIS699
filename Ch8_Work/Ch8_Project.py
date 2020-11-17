1a3c20cc6eeb6f4485846a2691de61585e31e6bb
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2020-11-17 16:00:53 -0500

    project updates

--- Ch8_Work/Ch8_Project.py
+++ Ch8_Work/Ch8_Project.py
@@ -20,11 +20,63 @@
 #can I use this type of clustering to do a type of idenficiation for paralogs vs orhtologs???
 
 # Links:
-#     https://biopython.org/docs/1.75/api/Bio.Cluster.html
-#     https://biopython.org/wiki/Phylo
-#     https://github.com/shubhamjha97/hierarchical-clustering
-#     https://www.tutorialspoint.com/biopython/biopython_cluster_analysis.htm
-#     https://www-ncbi-nlm-nih-gov.ezproxy.gvsu.edu/proteinclusters
-#     https://biopython-tutorial.readthedocs.io/en/latest/notebooks/13%20-%20Phylogenetics%20with%20Bio.Phylo.html
-#     https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
+    # https://biopython.org/docs/1.75/api/Bio.Cluster.html
+    # https://biopython.org/wiki/Phylo
+    # https://github.com/shubhamjha97/hierarchical-clustering
+    # https://www.tutorialspoint.com/biopython/biopython_cluster_analysis.htm
+    # https://www-ncbi-nlm-nih-gov.ezproxy.gvsu.edu/proteinclusters
+    # https://biopython-tutorial.readthedocs.io/en/latest/notebooks/13%20-%20Phylogenetics%20with%20Bio.Phylo.html
+
+
+#  this comes from https://github.com/kozo2/biopython-notebook/blob/nbsphinx/notebooks/13%20-%20Phylogenetics%20with%20Bio.Phylo.ipynb
+
+import Bio
+import copy
+from io import StringIO
+
+from Bio import Phylo
+from Bio.Phylo.Applications import PhymlCommandline
+from Bio.Phylo.PAML import codeml
+from Bio.Phylo.PhyloXML import Phylogeny
+
+tree = Phylo.read(r"C:\Users\Mikaela\Desktop\F20\CIS699\CIS699\CIS699\Ch8_Work\simple.dnd", "newick")
+
+print(tree)
+Phylo.draw_ascii(tree)
+
+tree.rooted = True
+Phylo.draw(tree)  # Colorless tree
+
+tree = tree.as_phyloxml()
+tree = Phylogeny.from_tree(tree)
+tree.root.color = (128, 128, 128)
+tree.root.color = "#808080"        # This is one alternative
+tree.root.color = "gray"           # This is another
+mrca = tree.common_ancestor({"name": "E"}, {"name": "F"})
+mrca.color = "salmon"
+tree.clade[0, 1].color = "blue"
+
+Phylo.draw(tree) #Colored tree
+
+#from Bio import Phylo
+tree = Phylo.read(r"C:\Users\Mikaela\Desktop\F20\CIS699\CIS699\CIS699\Ch8_Work\int_node_lables.nwk", "newick")
+# print(tree)
+
+trees = Phylo.parse(r"C:\Users\Mikaela\Desktop\F20\CIS699\CIS699\CIS699\Ch8_Work\phyloxml_examples.xml", "phyloxml")
+# trees = list(trees)
+# for tree in trees:
+#     print(tree)
+
+tree = Phylo.read(r"C:\Users\Mikaela\Desktop\F20\CIS699\CIS699\CIS699\Ch8_Work\example.xml", "phyloxml")
+# print(tree)
+
+# Phylo.draw_ascii(tree)
+# Phylo.draw(tree, branch_labels=lambda c: c.branch_length)
+
+#Sadly we need to monkey patch...
+# import networkx
+# from networkx.drawing import nx_agraph
+# networkx.graphviz_layout = nx_agraph.graphviz_layout
+
+# Phylo.draw_graphviz(tree, prog='dot')
 
